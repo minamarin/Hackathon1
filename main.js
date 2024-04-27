18d293cb65326c5cdcb13b91046d513b56270848
Stephany Ho <shohk39@gmail.com>
2024-04-26 23:05:45 -0700

    Update weather forecast feature

--- main.js
+++ main.js
@@ -1,27 +1,65 @@
 const input = document.querySelector("input");
 const searchBtn = document.querySelector("button");
 const output = document.querySelector("output");
-const backgroundElement = document.querySelector("body");
 
-searchBtn.addEventListener("click", async () => {
+async function getWeather(location) {
   try {
     const city_res = await fetch(
-      `https://api.weatherapi.com/v1/current.json?key=76ca8d66b5ae4c56991215645242603&q=${input.value}`
+      `https://api.weatherapi.com/v1/forecast.json?key=76ca8d66b5ae4c56991215645242603&q=${location}&days=3&aqi=no`
     );
     const city_data = await city_res.json();
+    const forecastArray = city_data.forecast.forecastday;
     console.log(city_data);
-
+    console.log(city_data.forecast.forecastday);
     output.innerHTML = `<div class="main">
-        <p class="city">${city_data.location.name}</p>
-        <p class="country">${city_data.location.country}</p>
-        <img src="https:${city_data.current.condition.icon}" alt="weather icon" id="weather-icon" />
-        <p class="condition">${city_data.current.condition.text}</p>
-        <p class="temp-f">${city_data.current.temp_f}°F</p>
-        <p>Gust mph: ${city_data.current.gust_mph}</p>
-        <p>UV: ${city_data.current.uv}</p>
-        <p>Local Time: ${city_data.location.localtime}</p>
+        <p class="location location-main">${city_data.location.name}</p>
+        <p class="location">${city_data.location.country}</p>
+        <p class="location">${city_data.location.localtime}</p>
+        <div class="current">
+        <p id="current-f">${city_data.current.temp_f}°</p>
+        <div>
+        <img src="https:${city_data.current.condition.icon}" alt="weather icon" class="weather-icon" id="current-weather-icon" />
+        </div>
+        </div>
+        <p class="current-condition">${city_data.current.condition.text}</p>
+        <div class="low-high current-condition"><span>L: ${forecastArray[0].day.mintemp_f}°</span><span class="low-high">H: ${forecastArray[0].day.maxtemp_f}°</span></div>
+        <br>
+
+        <div class="forecast">
+        <div class="daily-forecast">
+        <p>Today</p>
+        <img src="https:${forecastArray[0].day.condition.icon}" alt="weather icon" id="weather-icon" />
+        <p class="condition">${forecastArray[0].day.condition.text}</p>
+        <p class="low-f">Low: ${forecastArray[0].day.mintemp_f}°F</p>
+        <p class="high-f">High: ${forecastArray[0].day.maxtemp_f}°F</p>
+        </div>
+        
+        <div class="daily-forecast">
+        <p>Tomorrow</p>
+        <img src="https:${forecastArray[1].day.condition.icon}" alt="weather icon" id="weather-icon" />
+        <p class="condition">${forecastArray[1].day.condition.text}</p>
+        <p class="low-f">Low: ${forecastArray[1].day.mintemp_f}°F</p>
+        <p class="high-f">High: ${forecastArray[1].day.maxtemp_f}°F</p>
+        </div>
+
+        <div class="daily-forecast">
+        <p>Overmorrow</p>
+        <img src="https:${forecastArray[2].day.condition.icon}" alt="weather icon" id="weather-icon" />
+        <p class="condition">${forecastArray[2].day.condition.text}</p>
+        <p class="low-f">Low: ${forecastArray[2].day.mintemp_f}°F</p>
+        <p class="high-f">High: ${forecastArray[2].day.maxtemp_f}°F</p>
+        </div>
+        </div>
         </div>`;
+    input.value = "";
   } catch (error) {
-    console.error("An error occurred:", error.message);
+    // console.error("An error occurred:", error.message);
+    alert("Please enter a valid city or zip code and try again.");
   }
+}
+
+getWeather("Sacramento");
+
+searchBtn.addEventListener("click", async () => {
+  getWeather(input.value);
 });
